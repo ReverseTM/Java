88045ef1d0265bf3b5a72d5385b4ba6ab6ba9afb
Reverse <SteamGamesMy@mail.ru>
2023-09-20 20:40:45 +0300

    [Reverse] - implement interface Iterable and Iterator for BST

--- src/main/java/org/example/BinarySearchTree.java
+++ src/main/java/org/example/BinarySearchTree.java
@@ -1,36 +1,105 @@
 package org.example;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
 
-public final class BinarySearchTree<tkey, tvalue> extends AssociativeContainer<tkey, tvalue> {
-    private class Node {
+import org.javatuples.Triplet;
+
+public final class BinarySearchTree<tkey, tvalue>
+        extends AssociativeContainer<tkey, tvalue>
+        implements Iterable<Triplet<Integer, tkey, tvalue>> {
+
+    final class Node {
         private final Pair<tkey, tvalue> keyAndValue;
         Node leftSubtree;
         Node rightSubtree;
+
         public Node(tkey key, tvalue value) {
             keyAndValue = new Pair<>(key, value);
             leftSubtree = null;
             rightSubtree = null;
         }
     }
+
     final Comparator<tkey> comparator;
     Node root;
+
+    private final class TreeIterator implements Iterator<Triplet<Integer, tkey, tvalue>> {
+        private Node currentNode;
+        private final Stack<Node> way;
+
+        public TreeIterator(Node root) {
+            currentNode = root;
+            way = new Stack<>();
+            while (currentNode != null && currentNode.leftSubtree != null) {
+                way.push(currentNode);
+                currentNode = currentNode.leftSubtree;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentNode != null;
+        }
+
+        @Override
+        public Triplet<Integer, tkey, tvalue> next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            Triplet<Integer, tkey, tvalue> tuple = new Triplet<>(way.size(), currentNode.keyAndValue.first(), currentNode.keyAndValue.second());
+
+            if (currentNode.rightSubtree != null) {
+                way.push(currentNode);
+                currentNode = currentNode.rightSubtree;
+
+                while (currentNode.leftSubtree != null) {
+                    way.push(currentNode);
+                    currentNode = currentNode.leftSubtree;
+                }
+            } else {
+                if (!way.empty()) {
+                    if (way.peek().leftSubtree != currentNode) {
+                        while (!way.empty() && way.peek().rightSubtree == currentNode) {
+                            currentNode = way.pop();
+                        }
+                        if (way.empty()) {
+                            currentNode = null;
+                            return tuple;
+                        }
+
+                    }
+                    currentNode = way.pop();
+                } else {
+                    currentNode = null;
+                }
+            }
+            return tuple;
+        }
+    }
+
+    @Override
+    public Iterator<Triplet<Integer, tkey, tvalue>> iterator() {
+        return new TreeIterator(root);
+    }
 
     public BinarySearchTree(Comparator<tkey> comparator) {
         root = null;
         this.comparator = comparator;
     }
+
     private Node insertInner(Node node, tkey key, tvalue value) {
         if (node == null) {
             return new Node(key, value);
         }
         if (comparator.compare(node.keyAndValue.first(), key) > 0) {
             node.leftSubtree = insertInner(node.leftSubtree, key, value);
-        }
-        else if (comparator.compare(node.keyAndValue.first(), key) < 0) {
+        } else if (comparator.compare(node.keyAndValue.first(), key) < 0) {
             node.rightSubtree = insertInner(node.rightSubtree, key, value);
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Key already inserted");
         }
 
@@ -46,8 +115,8 @@ public final class BinarySearchTree<tkey, tvalue> extends AssociativeContainer<t
         }
 
         return comparator.compare(node.keyAndValue.first(), key) > 0
-            ? findInner(node.leftSubtree, key)
-            : findInner(node.rightSubtree, key);
+                ? findInner(node.leftSubtree, key)
+                : findInner(node.rightSubtree, key);
     }
 
     private Node findSmallestValue(Node node) {
@@ -79,8 +148,7 @@ public final class BinarySearchTree<tkey, tvalue> extends AssociativeContainer<t
 
         if (comparator.compare(node.keyAndValue.first(), key) > 0) {
             node.leftSubtree = removeInner(node.leftSubtree, key);
-        }
-        else {
+        } else {
             node.rightSubtree = removeInner(node.rightSubtree, key);
         }
 
@@ -98,8 +166,7 @@ public final class BinarySearchTree<tkey, tvalue> extends AssociativeContainer<t
         Node findNode = findInner(root, key);
         if (findNode != null) {
             return findNode.keyAndValue.second();
-        }
-        else {
+        } else {
             throw new NullPointerException("Key not found");
         }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,11 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        AssociativeContainer<Integer, Integer> bst = new BinarySearchTree<>(Comparator.comparingInt(left -> left));
-        bst.insert(10, 123);
-        bst.insert(4, 123421);
-        bst.insert(6, 312);
-        bst.insert(15, 31);
-        bst.insert(12, 3131);
-        bst.remove(10);
-        bst.remove(4);
-        bst.remove(15);
-        bst.remove(12);
-        bst.remove(6);
+        BinarySearchTree<Integer, Integer> tree = new BinarySearchTree<>(Comparator.comparingInt(left -> left));
+        tree.insert(10, 123).insert(4, 123421).insert(6, 312).insert(15, 31).insert(12, 3131);
+
+        for (var i : tree) {
+            System.out.println(i.getValue1());
+        }
     }
 }
